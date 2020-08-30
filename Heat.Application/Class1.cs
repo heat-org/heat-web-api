78b1c15a30487314c83911ec7e54029f179bc5c1
abraham <abrahamiduran@outlook.com>
2020-08-29 22:41:12 -0400

    Add missing projects

---
+++ Heat.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Heat.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Heat.Persistance/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Heat.Persistance
+{
+    public class Class1
+    {
+    }
+}
