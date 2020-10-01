e24c99d711ef33b5cfb924c2f7b4aa4284e298c3
César Lachapelle <cesarlachapelle05@gmail.com>
2020-10-01 11:20:09 -0400

    Update heatContext.cs (#7)

--- Heat.Persistance/Context/heatContext.cs
+++ Heat.Persistance/Context/heatContext.cs
@@ -46,11 +46,7 @@ namespace Heat.Persistance.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:heat.database.windows.net,1433;Initial Catalog=heat;Persist Security Info=False;User ID=sauron;Password=123456789q@Q;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
